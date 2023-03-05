6db78246e7466e15717ed03aad08d0546fbb8744
Fernando Avelar <fernandoestevaoa@gmail.com>
2023-03-05 12:25:20 -0300

    if

---
+++ if.js
@@ -0,0 +1,10 @@
+const idade = 29;
+if (idade >= 18) {
+    // é executado caso a condição seja verdadeira
+    console.log("É maior de idade");
+} else {
+    // É executado caso a condição seja falsa
+    console.log("É menor de idade");
+}
+
+console.log("Fim do código");
