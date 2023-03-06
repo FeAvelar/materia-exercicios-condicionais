bade67dc6c1a10aec09e7959e7c59faf20abd2d1
Fernando Avelar <fernandoestevaoa@gmail.com>
2023-03-06 20:39:32 -0300

    exercicio10

---
+++ exercicio10.js
@@ -0,0 +1,33 @@
+const caractere = "E";
+
+if (
+(caractere === "A") || 
+(caractere === "E") ||
+(caractere === "I") ||
+(caractere === "O") ||
+(caractere === "U") 
+) {
+    console.log("Vogal maiuscula");
+ } else if (
+    (caractere === "a") ||
+    (caractere === "e") ||
+    (caractere === "i") ||
+    (caractere === "o") ||
+    (caractere === "u") 
+) {
+    console.log("Vogal minuscula");
+} else if (
+        (caractere === 1) ||
+        (caractere === 2) ||
+        (caractere === 3) ||
+        (caractere === 4) ||
+        (caractere === 5) ||
+        (caractere === 6) ||
+        (caractere === 7) ||
+        (caractere === 8) ||
+        (caractere === 9) ||
+        (caractere === 0) 
+) {
+} else {
+      console.log("Este caractere é uma consoante")
+}
