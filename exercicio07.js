4d5f9f7b468bc5b79eb3af32bc1d9696b0a16ef0
Fernando Avelar <fernandoestevaoa@gmail.com>
2023-03-06 19:36:40 -0300

    exercicio07

---
+++ exercicio07.js
@@ -0,0 +1,11 @@
+const aposentada = false;
+const portadoraDeDoenca = false;
+const totalDeRendimentos = 3000000; 
+      //emCentavos
+ if (aposentada || portadoraDeDoenca) {
+    console.log("Isenta");
+ } else if (totalDeRendimentos < 2855970) {
+    console.log("Vaza leão! Já ta dificil sem você");
+ } else if (totalDeRendimentos >= 2855970) {
+    console.log("Pega Leao");
+ }
